9f24bfc3add02a84ebb123a0526bfd29dfca606d
Andrei Pfeiffer <andrei@upsidedown.ro>
2015-02-08 21:22:30 +0200

    Fix horizontal scroll

--- js/grayscale.js
+++ js/grayscale.js
@@ -1,24 +1,51 @@
-// jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 100) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
+var ANTAGON = (function() {
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
+    'use strict';
+
+    function init() {
+        scrollToSection();
+
+        // Closes the Responsive Menu on Menu Item Click
+        $('.navbar-collapse ul li a').click(function() {
+            $('.navbar-toggle:visible').click();
+        });
+    }
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    function scrollToSection() {
+        $('.page-scroll').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 800, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    }
+
+    // jQuery to collapse the navbar on scroll
+    function toggleMenu() {
+        if ($(".navbar").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        }
+    }
+
+    return {
+        init: init,
+        toggleMenu: toggleMenu
+    };
+
+})();
+
 $(function() {
-    $('.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 800, 'easeInOutExpo');
-        event.preventDefault();
+
+    'use strict';
+
+    ANTAGON.init();
+
+    $(window).scroll(function() {
+        ANTAGON.toggleMenu();
     });
 });
 
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
