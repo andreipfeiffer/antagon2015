96640a282b135eb468c94578f2967d29c17ca3f1
Andrei Pfeiffer <andrei@upsidedown.ro>
2015-10-07 10:19:01 +0300

    Update program

--- index.php
+++ index.php
@@ -268,7 +268,7 @@ $url['tvr']      = 'https://www.google.ro/maps/place/SRTv+-+TVR+Timisoara/@45.75
                             <li class="clearfix piesa">
                                 <strong class="nume">
                                     <span class="trupa">Unteatru <span class="oras">(București)</span></span>
-                                    <span class="transparent">Trădare</span>
+                                    <span class="transparent">Cinci ore cu Mario</span>
                                 </strong>
                                 <span class="locatie">
                                     <a href="<?=$url["jecza"]?>" target="_blank">Jecza Gallery</a>
